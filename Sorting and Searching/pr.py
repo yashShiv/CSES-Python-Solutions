375bcc030a97746efed9b3906374d4982f693c0d
Yashaswi Shivank <yashaswishivank@gmail.com>
2020-11-19 18:41:29 +0530

    initial commit

---
+++ Sorting and Searching/pr.py
@@ -0,0 +1,19 @@
+from collections import defaultdict
+
+n,sum = map(int,input().split())
+arr= list(map(int,input().split()))
+
+count = 0
+sum_history = defaultdict(lambda : 0)
+tempSum = 0
+ 
+for i in range(n):
+    tempSum += arr[i]
+    if tempSum == sum:
+        count += 1
+    if tempSum - sum in sum_history:
+        count += sum_history[tempSum - sum]
+        
+    sum_history[tempSum] += 1
+    
+print(count)
